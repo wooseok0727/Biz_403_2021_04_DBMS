30b0e62e6717f6eb91a62e4198830180e8f741c1
wooseok0727 <crash2@naver.com>
2021-04-30 07:09:07 +0900

    4월 29일

--- 도서대여 프로젝트/관리자(2021-04-29-도서대여).sql
+++ 도서대여 프로젝트/관리자(2021-04-29-도서대여).sql
@@ -2,7 +2,7 @@
 
 -- 프로젝트에서 사용할 TableSpace, User 생성
 CREATE TABLESPACE RentBookDB
-DATAFILE 'C:/oraclexe/date/rentbook.dbf'
+DATAFILE 'C:/oraclexe/data/rentbook.dbf'
 SIZE 1M AUTOEXTEND ON NEXT 1K;
 
 CREATE USER bookuser IDENTIFIED BY bookuser
