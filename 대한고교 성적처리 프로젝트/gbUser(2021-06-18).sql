629ed8fed73c3d0e9600cf65605b52b9d8abeb6e
wooseok0727 <crash2@naver.com>
2021-06-18 16:42:27 +0900

    6월 18일

---
+++ 대한고교 성적처리 프로젝트/gbUser(2021-06-18).sql
@@ -0,0 +1,68 @@
+CREATE DATABASE score;
+
+USE score;
+
+CREATE TABLE tbl_student (
+	st_num		CHAR(8)		PRIMARY KEY,
+	st_name		VARCHAR(20)	NOT NULL,	
+	st_dept		VARCHAR(20)	NOT NULL,	
+	st_grade	INT			NOT NULL,	
+	st_tel		VARCHAR(15)	NOT NULL,	
+	st_addr		VARCHAR(125)	
+);
+
+CREATE TABLE tbl_score (
+	sc_seq		CHAR(8)		PRIMARY KEY,
+	sc_stnum	CHAR(8)		NOT NULL,
+	sc_subject	VARCHAR(20)	NOT NULL,
+	sc_score	INT			NOT NULL	
+);
+
+ALTER TABLE tbl_score
+ADD CONSTRAINT fk_student
+FOREIGN KEY(sc_stnum)
+REFERENCES tbl_student(st_num);
+
+ALTER TABLE tbl_student
+ADD UNIQUE (st_name, st_dept,st_tel);
+
+SELECT * FROM tbl_student;
+SELECT * FROM tbl_score;
+
+INSERT INTO tbl_student (st_num, st_name,st_dept,st_grade,st_tel,st_addr)
+VALUES ("20210001","홍길동","컴퓨터공학",2,"010-555-5555","서울특별시");
+
+INSERT INTO tbl_student (st_num, st_name,st_dept,st_grade,st_tel,st_addr)
+VALUES ("20210002","홍길동","컴퓨터공학",2,"010-666-5555","서울특별시");
+
+INSERT INTO tbl_student (st_num, st_name,st_dept,st_grade,st_tel,st_addr)
+VALUES (20210003,"홍길동","컴퓨터공학",2,"010-655-5555","서울특별시");
+
+INSERT INTO tbl_score (sc_seq,sc_stnum,sc_subject,sc_score)
+VALUES ("0000001","20210001","수학",80);
+
+INSERT INTO tbl_score (sc_seq,sc_stnum,sc_subject,sc_score)
+VALUES ("0000002","20210001","국어",90);
+
+INSERT INTO tbl_score (sc_seq,sc_stnum,sc_subject,sc_score)
+VALUES ("0000003","20210002","국어",90);
+
+
+CREATE VIEW view_학생성적정보 AS 
+(
+	SELECT ST.st_num AS st_num,
+		   ST.st_name AS st_name,
+           ST.st_dept AS st_dept,
+           ST.st_grade AS st_grade,
+           COUNT(SC.sc_subject) AS sc_subject,
+		   SUM(SC.sc_score) AS sc_scores,
+           ROUND(AVG(SC.sc_score),1) AS sc_avg
+    FROM tbl_score AS SC
+		LEFT JOIN tbl_student AS ST
+			ON SC.sc_stnum = ST.st_num
+	GROUP BY ST.st_num
+);    
+SELECT * FROM view_학생성적정보;
+
+
+DROP VIEW view_학생성적정보;
