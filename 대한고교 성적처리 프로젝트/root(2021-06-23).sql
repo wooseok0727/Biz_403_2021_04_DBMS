863e39500b3bf1f94e936c0ef41557f24eec11e6
wooseok0727 <crash2@naver.com>
2021-06-25 16:08:52 +0900

    6월 25일

--- 대한고교 성적처리 프로젝트/root(2021-06-23).sql
+++ 대한고교 성적처리 프로젝트/root(2021-06-23).sql
@@ -67,3 +67,45 @@ INSERT INTO tbl_score(sc_stnum, sc_sbcode,sc_score)
 VALUES('20210001','S002',90);
 
 DESC tbl_student;
+
+/*
+tbl_subject, tbl_score table을 가지고
+각 학생의 성적 리스트를 출력해보기
+과목 리스트를 출력하고,
+각 과목의 성적이 입력된 학생의 리스트를 확인하기
+
+학번을 조건으로하여 한 학생의 성적입력 여부를 확인하기
+
+학생의 점수가 입력된 과목과 입력되지 않은 과목을
+확인하고 싶다
+*/
+-- subquery
+SELECT SB.sb_code, SB.sb_name, SB.sb_prof,
+	   SC.sc_stnum, SC.sc_score
+FROM tbl_subject SB
+	LEFT JOIN (SELECT * FROM tbl_score WHERE sc_stnum = '20210001') SC
+		ON SC.sc_sbcode = SB.sb_code;
+
+/*
+	과목리스트를 전체 보여주고,
+    학생의 성적 table을 JOIN하여
+    학생의 점수가 있으면 점수를 보이고
+    없으면 null을 보여주는 SQL JOIN문
+    
+    이 JOIN 명령문에
+    특정한 학번을 조건으로 부여하여
+    한 학생의 성적여부를 조회하는 SQL만들기
+    
+    순수한 JOIN을 사용하여 한 학생의 성적을 조회하는데
+    학생의 성적이 tbl_score table에 있으면 점수를 보이고
+    없으면 NULL 표현하기 위하여
+    WHERE 절에서 학번을 조건으로 부여하지 않고고
+    JOIN문의 ON 절에 학번을 조건으로 부여한다
+*/
+SELECT SB.sb_code, SB.sb_name, SB.sb_prof,
+	   SC.sc_stnum, SC.sc_score
+FROM tbl_subject SB
+	LEFT JOIN tbl_score SC
+		ON SC.sc_sbcode = SB.sb_code
+        AND SC.sc_stnum = '20210001' LIMIT 5;
+
