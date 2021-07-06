14326af7ea4b62304bb3b56e4948ca55a9d2c677
wooseok0727 <crash2@naver.com>
2021-07-06 15:13:30 +0900

    7월 6일

--- 이미지갤러리/imageGallery(2021-07-06).sql
+++ 이미지갤러리/imageGallery(2021-07-06).sql
@@ -14,3 +14,14 @@ SHOW TABLES;
 
 DROP TABLE tbl_gallery;
 
+CREATE TABLE tbl_files (
+	file_seq		BIGINT		AUTO_INCREMENT	PRIMARY KEY,
+	file_gseq		BIGINT		NOT NULL,	
+	file_originname	VARCHAR(125)	NOT NULL,
+	file_upname		VARCHAR(125)	NOT NULL	
+);
+
+SHOW TABLES;
+DROP TABLE tbl_files;
+
+SELECT * FROM tbl_gallery;
