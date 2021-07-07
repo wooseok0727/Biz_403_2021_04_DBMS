0f44969f7cce8af2faf1388054dc4d8546363dae
wooseok0727 <crash2@naver.com>
2021-07-07 15:06:08 +0900

    7월 7일

--- 이미지갤러리/imageGallery(2021-07-06).sql
+++ 이미지갤러리/imageGallery(2021-07-06).sql
@@ -25,8 +25,53 @@ SHOW TABLES;
 DROP TABLE tbl_files;
 
 SELECT * FROM tbl_gallery;
+SELECT * FROM tbl_files;
 
 -- 현재 연결된 session에서 INSERT가 수행되고
 -- 그 과정에서 AUTO_INCREMENT 컬럼이 변화가 있으면
 -- 그 값을 알려주는 함수
 SELECT LAST_INSERT_ID();
+
+DESC tbl_gallery;
+
+INSERT INTO tbl_files(file_gseq, file_originname, file_upname)
+VALUES
+(1,"title1",'uuid-title'),
+(1,"title2",'uuid-title'),
+(1,"title3",'uuid-title');
+
+-- INSERT를 수행할때
+-- AUTO_INCREMENT로 설정된 칼럼에
+-- 0 또는 null, ''등을 설정하면
+-- AUTO_INCREMENT가 작동된다
+
+INSERT INTO tbl_gallery (g_seq,g_writer,g_date,g_time,g_subject,g_content)
+VALUES ( 0, 'ws','2021-01-01','12:00:00','제목','내용');
+
+-- EQ JOIN
+-- 카디션 곱
+-- 두개의 table을 JOIN하여
+-- table1 개수 * table2 개수 만큼 list 출력
+SELECT * FROM tbl_gallery G, tbl_files F
+WHERE G.g_seq = F.file_gseq AND G.g_seq = 1;
+
+CREATE VIEW view_갤러리 AS
+(
+SELECT G.g_seq AS g_seq, 
+	   G.g_writer AS g_writer, 
+       G.g_date AS g_date, 
+       G.g_time AS g_time, 
+       G.g_subject AS g_subject, 
+       G.g_content AS g_content, 
+       G.g_image AS g_image, 
+       F.file_seq AS f_seq,
+       F.file_originname AS f_originname, 
+       F.file_upname AS f_upname
+FROM tbl_gallery G, tbl_files F
+WHERE G.g_seq = F.file_gseq
+);
+
+DROP VIEW view_갤러리;
+DESC view_갤러리;
+
+SELECT * FROM view_갤러리;
--- 하루생각/gbUser(하루생각).sql
+++ 하루생각/gbUser(하루생각).sql
@@ -3,31 +3,42 @@ CREATE DATABASE amind;
 use amind;
 
 CREATE TABLE tbl_user (
-	u_seq	BIGINT	auto_increment	PRIMARY KEY,
-	u_prof	BIGINT	NOT NULL,	
+	u_seq	BIGINT			auto_increment	PRIMARY KEY,
+	u_prof	BIGINT,		
 	u_id	VARCHAR(125)	NOT NULL	UNIQUE,
 	u_pw	VARCHAR(125)	NOT NULL,	
 	u_nick	VARCHAR(125)	NOT NULL	UNIQUE,
 	u_mail	VARCHAR(125)	NOT NULL	UNIQUE,
-	u_warning	INT	
+	u_warning	INT		
 );
+
+INSERT INTO tbl_user(u_id,u_pw,u_nick,u_mail)
+VALUES('korea','12345','홍길동','abc@gmail.com');
+
+INSERT INTO tbl_user(u_id,u_pw,u_nick,u_mail)
+VALUES('korea1','12345','박길동','abc1@gmail.com');
+
+INSERT INTO tbl_user(u_id,u_pw,u_nick,u_mail)
+VALUES('korea2','12345','김길동','abc2@gmail.com');
+
+INSERT INTO tbl_user(u_id,u_pw,u_nick,u_mail)
+VALUES('korea3','12345','최길동','abc3@gmail.com');
 
 CREATE TABLE tbl_userProf (
 	prof_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
-	prof_user	BIGINT	NOT NULL,	
 	prof_url	VARCHAR(300)		
 );
 
 CREATE TABLE tbl_writing (
-	wr_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
-	wr_user	BIGINT	NOT NULL,	
-	wr_nick	VARCHAR(50)	NOT NULL,	
-	wr_content	VARCHAR(3000)	NOT NULL,
-	wr_like_count	INT,		
-	wr_warning_count	INT,		
-	wr_write_date		TIMESTAMP DEFAULT NOW(),
-	wr_last_date		TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	wr_origin	VARCHAR(50)		
+	wr_seq				BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+	wr_user				BIGINT	NOT NULL,	
+	wr_nick				VARCHAR(50)	NOT NULL,	
+	wr_content			VARCHAR(3000)	NOT NULL,	
+	wr_like_count		INT,		
+	wr_warning_count	INT,	
+	wr_write_date		TIMESTAMP DEFAULT NOW(),	
+	wr_last_date		TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,	
+	wr_origin			VARCHAR(50)		
 );
 
 CREATE TABLE tbl_warning (
@@ -35,16 +46,57 @@ CREATE TABLE tbl_warning (
 	wa_writing	BIGINT	NOT NULL,	
 	wa_user	BIGINT	NOT NULL,	
 	wa_content	VARCHAR(3000)	NOT NULL,	
-	wa_check	TINYINT(1),		
+	wa_check	INT,		
 	wa_date	TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE tbl_like (
-	li_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+	li_seq		BIGINT	AUTO_INCREMENT	PRIMARY KEY,
 	li_wr_seq	BIGINT	NOT NULL,	
-	li_fan	BIGINT	NOT NULL,	
-	li_date	TIMESTAMP DEFAULT NOW(), 		
-	li_check	TINYINT(2)		
+	li_fan		BIGINT	NOT NULL,	
+	li_date		TIMESTAMP DEFAULT NOW()		
 );
 
 SELECT * FROM tbl_writing;
+SELECT * FROM tbl_user;
+DELETE FROM tbl_user 
+WHERE u_seq = 1;
+
+SELECT * FROM tbl_writing
+ORDER BY RAND() LIMIT 1;
+
+DROP TABLE tbl_user;
+DROP TABLE tbl_userProf;
+DROP TABLE tbl_writing;
+DROP TABLE tbl_warning;
+DROP TABLE tbl_like;
+
+ALTER TABLE tbl_user
+ADD CONSTRAINT fk_user_prof
+FOREIGN KEY (u_prof)
+REFERENCES tbl_userprof(prof_seq);
+
+ALTER TABLE tbl_writing
+ADD CONSTRAINT fk_wr_user
+FOREIGN KEY(wr_user)
+REFERENCES tbl_user(u_seq);
+
+ALTER TABLE tbl_warning
+ADD CONSTRAINT fk_wa_writing
+FOREIGN KEY(wa_writing)
+REFERENCES tbl_writing(wr_seq);
+
+ALTER TABLE tbl_warning
+ADD CONSTRAINT fk_wa_user
+FOREIGN KEY(wa_user)
+REFERENCES tbl_user(u_seq);
+
+ALTER TABLE tbl_like
+ADD CONSTRAINT fk_li_writing
+FOREIGN KEY(li_wr_seq)
+REFERENCES tbl_writing(wr_seq);
+
+ALTER TABLE tbl_like
+ADD CONSTRAINT fk_li_user
+FOREIGN KEY(li_fan)
+REFERENCES tbl_user(u_seq);
